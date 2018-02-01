30ba1c4f70a32b809d6a863271df14484c49f57f
Anna <anna.gavrilova@georgebrown.ca>
2018-02-01 11:35:41 -0500

    Kill me

---
+++ src/copy/task.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./task.css";
+
+/*const task={
+	title:"Title",
+	completed:false
+}
+
+<Task task={task}/>*/
+
+class Task extends React.Component{
+
+	render(){
+
+		const title=this.props.task.title;
+		let completed=this.props.task.completed;
+		let completedClassName=(!completed)?"task":"completedTask"
+		return(
+			<div className={completedClassName}>
+				{title}
+			</div>
+			);
+	}
+}
+
+export default Task;
